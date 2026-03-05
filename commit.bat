d0dd3581a033bb06c288cc702a9e3b596d850753
JulerGT <124395008+JulerGaming@users.noreply.github.com>
2026-03-05 08:10:50 -0400

    hi

---
+++ commit.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d %~dp0
+
+echo Adding changes...
+git add .
+
+set /p msg=Enter commit message: 
+
+echo Committing...
+git commit -m "%msg%"
+
+echo Pushing to GitHub...
+git push
+
+echo Done!
+pause
