b8429209ca25d9ff49471427d5c4ab02eb339789
JulerGT <124395008+JulerGaming@users.noreply.github.com>
2026-06-16 01:19:56 -0400

    commit

--- index.js
+++ index.js
@@ -2459,7 +2459,7 @@ client.on("interactionCreate", async (interaction) => {
                 }
             }
             if (interaction.commandName.includes("voice") || interaction.commandName.includes("join") || interaction.commandName.includes("openurlstream")) {
-                if (blockedChannels.includes(interaction.channelID)) {
+                if (blockedChannels.includes(interaction.member.voice.channel.id)) {
                     return interaction.reply({ ephemeral: true, content: 'Cannot join this channel!' })
                 }
             }
